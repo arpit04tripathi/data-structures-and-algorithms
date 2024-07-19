38ed41bae718d252d4f31773fdafefbf48352525
Arpit Tripathi <arpit04tripathi@gmail.com>
2024-07-19 21:20:39 +0100

    update common Main class

--- common/src/main/java/com/a04t/dsa/common/Main.java
+++ common/src/main/java/com/a04t/dsa/common/Main.java
@@ -2,7 +2,10 @@ package com.a04t.dsa.common;
 
 public class Main {
     public static void main(String[] args) {
+        String divider = "=============================================";
+        System.out.println(divider);
         System.out.println("Hello world!");
-        System.out.println("started Main in com.a04t.dsa.common!");
+        System.out.println("started Main in com.a04t.dsa.common");
+        System.out.println(divider);
     }
 }
