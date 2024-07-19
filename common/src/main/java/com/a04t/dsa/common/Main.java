70d8b27609e27e191d28c48174138ce8cb026e6a
Arpit Tripathi <32494300+arpit04tripathi@users.noreply.github.com>
2024-07-19 21:42:41 +0100

    Init gradle (#1)
    * gradle init application
    * add readme
    * add modules: common, data-structures and algorithms

---
+++ common/src/main/java/com/a04t/dsa/common/Main.java
@@ -0,0 +1,11 @@
+package com.a04t.dsa.common;
+
+public class Main {
+    public static void main(String[] args) {
+        String divider = "=============================================";
+        System.out.println(divider);
+        System.out.println("Hello world!");
+        System.out.println("started Main in com.a04t.dsa.common");
+        System.out.println(divider);
+    }
+}
---
+++ data-structures/src/main/java/com/a04t/dsa/datastructures/Main.java
@@ -0,0 +1,11 @@
+package com.a04t.dsa.datastructures;
+
+public class Main {
+    public static void main(String[] args) {
+        String divider = "=============================================";
+        System.out.println(divider);
+        System.out.println("Hello world!");
+        System.out.println("started Main in com.a04t.dsa.datastructures");
+        System.out.println(divider);
+    }
+}
