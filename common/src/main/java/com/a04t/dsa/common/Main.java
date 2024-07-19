99ba0d878dd878046eade18c000dc7ad05e6a04b
Arpit Tripathi <arpit04tripathi@gmail.com>
2024-07-19 20:47:02 +0100

    add common

---
+++ common/src/main/java/com/a04t/dsa/common/Main.java
@@ -0,0 +1,8 @@
+package com.a04t.dsa.common;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("started Main in com.a04t.dsa.common!");
+    }
+}
