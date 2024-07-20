b93a300855e034f461118fce3d53494f75f2960a
Arpit Tripathi <arpit04tripathi@gmail.com>
2024-07-20 22:30:27 +0100

    update Singly Linked List

--- data-structures/src/main/java/com/a04t/dsa/datastructures/list/SinglyLinkedList.java
+++ data-structures/src/main/java/com/a04t/dsa/datastructures/list/SinglyLinkedList.java
@@ -2,8 +2,10 @@ package com.a04t.dsa.datastructures.list;
 
 import com.a04t.dsa.common.list.SLLNode;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class SinglyLinkedList<T> {
 
     SLLNode<T> head;
@@ -17,8 +19,22 @@ public class SinglyLinkedList<T> {
         return this.length;
     }
 
+    private boolean isEmptyList() {
+        return head == null;
+    }
+
+    private boolean isLastNode(SLLNode<T> node) {
+        return node.getNext() == null;
+    }
+
+    /**
+     * ======================================
+     * Get Node
+     * ======================================
+     **/
+
     public SLLNode<T> getLastNode() {
-        if (head == null) return null;
+        if (isEmptyList()) return null;
         SLLNode<T> lastNode = head;
         while (lastNode.getNext() != null) {
             lastNode = lastNode.getNext();
@@ -26,21 +42,186 @@ public class SinglyLinkedList<T> {
         return lastNode;
     }
 
-    public void addAtStart(T data) {
+    public SLLNode<T> getNodeAt(int index) {
+        if (isEmptyList() || (index >= size())) {
+            return null;
+        }
+        SLLNode<T> selectedNode = head;
+        for (int i = 0; i < index; i++) {
+            selectedNode = selectedNode.getNext();
+        }
+        return selectedNode;
+    }
+
+    /**
+     * ======================================
+     * Insert Node
+     * ======================================
+     **/
+
+    public void insertAtStart(T data) {
         SLLNode<T> newNode = new SLLNode<>(data);
-        if (size() > 0) {
+        if (!isEmptyList()) {
             newNode.setNext(head);
         }
         head = newNode;
         length++;
     }
 
-    public void addAtEnd(T data) {
+    public void insertAtEnd(T data) {
         SLLNode<T> newNode = new SLLNode<>(data);
-        if (head == null) {
+        if (isEmptyList()) {
             head = newNode;
+            length++;
             return;
         }
         getLastNode().setNext(newNode);
+        length++;
+    }
+
+    public void insertAt(int index, T data) {
+        if (index == 0) {
+            insertAtStart(data);
+        } else if (index >= size()) {
+            insertAtEnd(data);
+        } else {
+            SLLNode<T> newNode = new SLLNode<>(data);
+            SLLNode<T> previousNode = getNodeAt(index - 1);
+            newNode.setNext(previousNode.getNext());
+            previousNode.setNext(newNode);
+        }
+        length++;
+    }
+
+    /**
+     * ======================================
+     * Delete Node
+     * ======================================
+     **/
+
+    public void deleteAtStart() {
+        if (!isEmptyList()) {
+            head = head.getNext();
+            length--;
+        }
+    }
+
+    public void deleteAtEnd() {
+        if (size() <= 1) {
+            deleteAtStart();
+        } else {
+            int lastIndex = size() - 1;
+            SLLNode<T> secondLastNode = getNodeAt(lastIndex - 1);
+            secondLastNode.setNext(null);
+            length--;
+        }
+    }
+
+    public void deleteAt(int index) {
+        if (index == 0) {
+            deleteAtStart();
+        } else if (index >= size() - 1) {
+            deleteAtEnd();
+        } else {
+            SLLNode<T> previousNode = getNodeAt(index - 1);
+            SLLNode<T> nodeAtIndex = previousNode.getNext();
+            previousNode.setNext(nodeAtIndex.getNext());
+            length--;
+        }
+    }
+
+    /**
+     * ======================================
+     * Reverse the List
+     * ======================================
+     **/
+    public void reverseTheListIterative() {
+        if (size() <= 1) {
+            return;
+        }
+        SLLNode<T> previousNode = null;
+        SLLNode<T> currentNode = head;
+        SLLNode<T> nextNode = currentNode.getNext();
+        while (!isLastNode(currentNode)) {
+            currentNode.setNext(previousNode);
+            previousNode = currentNode;
+            currentNode = nextNode;
+            nextNode = currentNode.getNext();
+        }
+        currentNode.setNext(previousNode);
+        head = currentNode;
+    }
+
+    public void reverseTheListRecursive() {
+        if (size() <= 1) {
+            return;
+        }
+        reverseTheListRecursive(head);
+    }
+
+    private void reverseTheListRecursive(SLLNode<T> currentNode) {
+        // terminating the recursion when last node reached
+        if (isLastNode(currentNode)) {
+            // last node will be the head for reversed list
+            head = currentNode;
+            return;
+        }
+
+        // reverse the list from next node
+        reverseTheListRecursive(currentNode.getNext());
+
+        // reverse the pointers for current node
+        SLLNode<T> nextNode = currentNode.getNext();
+        nextNode.setNext(currentNode);
+        currentNode.setNext(null);
+    }
+
+    /**
+     * ======================================
+     * Print the List
+     * ======================================
+     **/
+
+    // 1 --> 2 --> 3 --> 4 --> null
+    public void print() {
+        SLLNode<T> currentNode = head;
+        StringBuilder sb = new StringBuilder();
+        while (currentNode != null) {
+            sb.append(currentNode.getData()).append(" --> ");
+            currentNode = currentNode.getNext();
+        }
+        sb.append("null");
+        System.out.println(sb.toString());
+    }
+
+    // 1 --> 2 --> 3 --> 4 --> null
+    public void printForwardRecursive(SLLNode<T> currentNode) {
+        if (currentNode == null) {
+            System.out.print("null");
+            return;
+        }
+
+        System.out.printf("%s --> ", currentNode.getData().toString());
+        printForwardRecursive(currentNode.getNext());
+    }
+
+    // null --> 4 --> 3 --> 2 --> 1
+    public void printReverseRecursive(SLLNode<T> currentNode) {
+        if (currentNode == null) {
+            System.out.print("null");
+            return;
+        }
+
+        printReverseRecursive(currentNode.getNext());
+        System.out.printf(" --> %s", currentNode.getData().toString());
+    }
+
+    public void debugPrint() {
+        System.out.println("=============================================");
+        print();
+        printForwardRecursive(head);
+        System.out.println();
+        printReverseRecursive(head);
+        System.out.println("\n=============================================");
     }
 }
