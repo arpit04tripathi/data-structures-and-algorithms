0bd4fdc84d17729b7335bfd1b0e968d7427bb8c9
Arpit Tripathi <arpit04tripathi@gmail.com>
2025-09-17 11:10:37 +0100

    add Main classed

---
+++ data-structures/src/main/java/com/a04t/dsa/ds/Main.java
@@ -0,0 +1,13 @@
+package com.a04t.dsa.ds;
+
+public class Main {
+
+    public static final String DIVIDER = "=============================================";;
+
+    public static void main(String[] args) {
+        System.out.println(DIVIDER);
+        System.out.println("Hello world!");
+        System.out.println("started Main in com.a04t.dsa.ds");
+        System.out.println(DIVIDER);
+    }
+}
