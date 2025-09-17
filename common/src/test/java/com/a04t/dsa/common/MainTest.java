67df53d3b180a0f63cb6368431dfb67eb45ba1fa
Arpit Tripathi <arpit04tripathi@gmail.com>
2025-09-17 11:13:25 +0100

    common: add unit test

---
+++ common/src/test/java/com/a04t/dsa/common/MainTest.java
@@ -0,0 +1,21 @@
+package com.a04t.dsa.common;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test_main() {
+        Main app = new Main();
+    }
+}
